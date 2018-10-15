f5438798ee29391a6955ac197afaacad72fb196a
selfup <boudinot.regis@yahoo.com>
2018-10-14 22:03:53 -0500

    improve ruby script for new posts

--- rubies/new_micro.rb
+++ rubies/new_micro.rb
@@ -1,17 +1,24 @@
+# To use
+
+# ruby rubies/new_micro.rb Some-title-with-hyphens
+# now a proper file name with enough boilerplate will be created
+
 current_date = Time.now.to_s.split(' ')[0]
 
 title = ARGV[0]
+post_title = title.split('-').join(' ')
+post_title_name = title.downcase
 
 default = "---
 layout: post
-title:  #{title}
+title:  #{post_title}
 ---
 
-# Title
+# #{post_title}
 
 Content
 "
 
-file = "./_posts/#{current_date}-#{title}.md"
+file = "_posts/#{current_date}-#{post_title_name}.md"
 
 File.write(file, default)
